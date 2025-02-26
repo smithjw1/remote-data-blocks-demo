bbcdebc38477b40a0e600de96435806f4a9ef9fb
Jacob Wayne Smith <jacob.smith@automattic.com>
2025-02-26 14:39:58 -0800

    seeing what breaks things

--- rdb-example.php
+++ rdb-example.php
@@ -95,9 +95,11 @@ function register_airtable_data_source(): void
         $result['uuid']
     ));
 
+    /*
     // Register Airtable-specific blocks
     AirtableIntegration::register_blocks_for_airtable_data_source($result);
     AirtableIntegration::register_loop_blocks_for_airtable_data_source($result);
+    */
 }
 
 // Register the data source when WordPress initializes
